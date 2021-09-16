db0d9594de592ea8f72e272411b03bad3d90791a
Kiwi ̄ツ <85539149+kiwidev0@users.noreply.github.com>
2021-09-16 23:31:09 +0000

    .

---
+++ commands/eval.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "eval",
+  code: `
+$onlyForIDs[834459205205229569;<:failed:887822581061804102> Você Não Tem Permissão]
+$eval[$message]
+`
+} 
--- commands/parceiro.js
+++ commands/parceiro.js
@@ -1,7 +1,8 @@
 module.exports = {
-  name: "parceria",
+  name: "pav",
   code: `
 @here
+$onlyForRoles[888202431928881192;<:failed:887822581061804102> | Você Não e Da Equipe De Parceira Astro]
 $title[Uma Nova Parceira Apareceu]
 $description[
 Representante:$message
@@ -10,7 +11,9 @@ Responsável:<@$authorID>
 
 $image[https://media.discordapp.net/attachments/887798469832605750/888174687824269322/standard.gif]
 ]
+$argsCheck[>1;<:failed:887822581061804102> |  Mencione Alguém]
 $color[303136]
+$deletecommand
 $footer[Equipe Parceira Astro]
 `
 } 
